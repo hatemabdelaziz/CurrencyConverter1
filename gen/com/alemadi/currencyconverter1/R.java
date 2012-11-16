294b5c32d05cbd3d6052e7d42ecd186d108ddadd
hatem <ha_lort@hotmail.com>
2012-11-16 23:33:19 +0300

    some layout changes

--- gen/com/alemadi/currencyconverter1/R.java
+++ gen/com/alemadi/currencyconverter1/R.java
@@ -8,29 +8,40 @@
 package com.alemadi.currencyconverter1;
 
 public final class R {
+    public static final class array {
+        public static final int name=0x7f050000;
+        public static final int value=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int btnSubmit=0x7f070003;
-        public static final int editText1=0x7f070000;
-        public static final int menu_settings=0x7f070005;
-        public static final int spinner1=0x7f070001;
-        public static final int spinner2=0x7f070002;
-        public static final int textView1=0x7f070004;
+        public static final int button1=0x7f080008;
+        public static final int relativeLayout1=0x7f080005;
+        public static final int relativeLayout2=0x7f080006;
+        public static final int spinner1=0x7f080001;
+        public static final int spinner2=0x7f080003;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080002;
+        public static final int textView3=0x7f080004;
+        public static final int textView4=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f040006;
+        public static final int calculate=0x7f040005;
+        public static final int equal=0x7f040003;
+        public static final int hello=0x7f040000;
+        public static final int in=0x7f040002;
+        public static final int one=0x7f040001;
+        public static final int result=0x7f040004;
     }
     public static final class style {
         /** 
@@ -54,10 +65,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/com/alemadi/currencyconverter1/MainActivity.java
+++ src/com/alemadi/currencyconverter1/MainActivity.java
@@ -1,22 +1,124 @@
 package com.alemadi.currencyconverter1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	public int to;
+	public int from;
+	public String [] val;
+	public String s;
+	public Handler handler;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); 
+        setContentView(R.layout.activity_main);
+        Spinner s1 = (Spinner) findViewById(R.id.spinner1);
+        Spinner s2 = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.name, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
+        val  = getResources().getStringArray(R.array.value);
+        s1.setAdapter(adapter);
+        s2.setAdapter(adapter);
+        s1.setOnItemSelectedListener(new spinOne(1));
+        s2.setOnItemSelectedListener(new spinOne(2));
+        Button b = (Button) findViewById(R.id.button1);
+        b.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				TextView t = (TextView) findViewById(R.id.textView4);	
+				if(from == to)
+				{
+					Toast.makeText(getApplicationContext(), "Invalid", 4000).show();
+				}
+				else
+				{										
+					  try {
+						 s = getJson("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22"+val[from]+val[to]+"%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=");	                    
+						JSONObject jObj;
+						jObj = new JSONObject(s);
+						String exResult = jObj.getJSONObject("query").getJSONObject("results").getJSONObject("rate").getString("Rate");
+						t.setText(exResult);
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						catch (ClientProtocolException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}													 
+					}											
+				}							 	
+        });
+    }
+    public String getJson(String url)throws ClientProtocolException, IOException {
+    	
+		StringBuilder build = new StringBuilder();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(url);
+	    HttpResponse response = client.execute(httpGet);
+		HttpEntity entity = response.getEntity();
+		InputStream content = entity.getContent();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+		String con;
+		while ((con = reader.readLine()) != null) {
+					build.append(con);
+				}
+		return build.toString();
 	}
+    private class spinOne implements OnItemSelectedListener
+    {
+    	int ide;
+    	spinOne(int i)
+    	{
+    		ide =i;
+    	}
+    	public void onItemSelected(AdapterView<?> parent, View view,
+    			int index, long id) {
+    		if(ide == 1)
+    			from = index;
+    		else if(ide == 2)
+    			to = index;
+    			
+    	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+    	public void onNothingSelected(AdapterView<?> arg0) {
+    		// TODO Auto-generated method stub	
+    	}
+    	
+    }
 
 }
